87bd4ed6662abb6d892e2f645dd6a9e45f515713
Nikolas Railan <nikolasrailanmp@gmail.com>
2025-05-22 08:40:32 -0300

    update

--- desweb/atv9dw/scripts/regional.js
+++ desweb/atv9dw/scripts/regional.js
@@ -2,34 +2,34 @@ const data_regional = []
 
 
 function addRegionals() {
-
+    
     // Obtém referência do formulário
     const form = document.querySelector('#formRegional')
     // Obtém o valor dos campos de entrada
     const acronym_name = form.querySelector('input[name="sigla"]')
-
+    
     const city_name = form.querySelector('input[name="cidade"]')
-
-
+    
+    
     const row = {
         id: data_regional.length + 1,
         acronym: acronym_name.value,
         city: city_name.value
     }
-
+    
     data_regional.push(row);
-
+    
     // Chama a função para criar a linha na tabela
     createRowRegional(row);
 
     // Limpa o campo do formulário após adicionar
     form.reset();
 
-    console.log("Deu certo ou nao rs");
 }
 
 
-function createRow(row) {
+function createRowRegional  (row) {
+
     // Obtém referência da tabela
     const table = document.querySelector('#table_regional').querySelector('tbody');
     // Cria um elemento <tr>
@@ -37,14 +37,14 @@ function createRow(row) {
     // Cria elementos <td> para cada coluna
     const col_id = document.createElement('td');
     col_id.textContent = row.id; // Adiciona o ID
-
+    
     const col_sigla = document.createElement('td');
     col_sigla.textContent = row.acronym; // Adiciona o nome do assunto]
-
+    
     const col_cidade = document.createElement('td');
     col_cidade.textContent = row.city; // Adiciona o nome do assunto
-
-
+    
+    
     // Adiciona os elementos <td> ao <tr>
     line.appendChild(col_id);
     line.appendChild(col_sigla);
@@ -52,4 +52,15 @@ function createRow(row) {
 
     // Adiciona a linha <tr> à tabela <tbody>
     table.appendChild(line);
+
+    addtoSelect(row.acronym, row.city);
+}
+
+
+function addtoSelect(acronym, city) {
+    const select = document.querySelector('#select_regional');
+    const op = document.createElement("option");
+    op.textContent = acronym;
+    op.value = city;
+    select.appendChild(op);
 }
