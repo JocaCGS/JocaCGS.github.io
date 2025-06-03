6d31e0d95cc9bf3c3898e9914833a4399429f901
JocaCGS <joaquimcolini06@hotmail.com>
2025-06-03 13:49:59 -0300

    update

--- desweb/atv9dw/scripts/regional.js
+++ desweb/atv9dw/scripts/regional.js
@@ -1,66 +1,43 @@
-const data_regional = []
 
+const data_regionals = []
 
-function addRegionals() {
-    
-    // Obtém referência do formulário
+function addRegional() {
+    const i = data_regionals.length;
+    const regional = {}
+
     const form = document.querySelector('#formRegional')
-    // Obtém o valor dos campos de entrada
-    const acronym_name = form.querySelector('input[name="sigla"]')
-    
-    const city_name = form.querySelector('input[name="cidade"]')
-    
-    
-    const row = {
-        id: data_regional.length + 1,
-        acronym: acronym_name.value,
-        city: city_name.value
-    }
-    
-    data_regional.push(row);
-    
-    // Chama a função para criar a linha na tabela
-    createRowRegional(row);
+    const input_nome = form.querySelector('input[name="sigla"]')
+    regional.sigla = input_nome
+    const input_nome_cidade = form.querySelector('input[name="cidade"]')
+    regional.cidade = input_nome_cidade
+    const id = i+1;
 
-    // Limpa o campo do formulário após adicionar
-    form.reset();
-
-}
+    regional.id = id
 
 
-function createRowRegional  (row) {
-
-    // Obtém referência da tabela
-    const table = document.querySelector('#table_regional').querySelector('tbody');
-    // Cria um elemento <tr>
+    const table = document.querySelector('#table_regional');
     const line = document.createElement('tr')
-    // Cria elementos <td> para cada coluna
-    const col_id = document.createElement('td');
-    col_id.textContent = row.id; // Adiciona o ID
+    const col_id = document.createElement('td')  
+    col_id.textContent = id
+    const col_nome = document.createElement('td')    
+    col_nome.textContent = input_nome.value
+    const col_nome_cidade = document.createElement('td')
+    col_nome_cidade.textContent = input_nome_cidade.value 
+
     
-    const col_sigla = document.createElement('td');
-    col_sigla.textContent = row.acronym; // Adiciona o nome do assunto]
-    
-    const col_cidade = document.createElement('td');
-    col_cidade.textContent = row.city; // Adiciona o nome do assunto
-    
-    
-    // Adiciona os elementos <td> ao <tr>
     line.appendChild(col_id);
-    line.appendChild(col_sigla);
-    line.appendChild(col_cidade);
+    line.appendChild(col_nome);
+    line.appendChild(col_nome_cidade)
 
-    // Adiciona a linha <tr> à tabela <tbody>
     table.appendChild(line);
+    data_regionals.push(regional)
+    drawChart();
 
-    addtoSelect(row.acronym, row.city);
-}
-
-
-function addtoSelect(acronym, city) {
     const select = document.querySelector('#select_regional');
-    const op = document.createElement("option");
-    op.textContent = acronym;
-    op.value = city;
-    select.appendChild(op);
+    const op = document.createElement("option");     
+    op.textContent = input_nome.value
+    op.value = input_nome.value
+    select.appendChild(op)
+
+    form .reset()
 }
--- desweb/atv9dw/scripts/subject.js
+++ desweb/atv9dw/scripts/subject.js
@@ -1,49 +1,37 @@
+
 const data_subjects = []
 
 
-
 function addSubject() {
+    const i = data_subjects.length;
+    const assunto = {}
 
-
-    alert("oi");
-    // Obtém referência do formulário
     const form = document.querySelector('#formSubject')
-    // Obtém o valor dos campos de entrada
-    const subject_name = form.querySelector('input[name="descricao"]')
+    const input_nome = form.querySelector('input[name="descricao"]')
+    const id = i+1;
+
+    assunto.nome = input_nome
+    assunto.id = id
 
 
-    const row = {
-        id: data_subjects.length + 1,
-        name: subject_name.value
-    }
-
-    data_subjects.push(row);
-
-    // Chama a função para criar a linha na tabela
-    createRowSubject(row);
-
-    // Limpa o campo do formulário após adicionar
-    form.reset();
-}
-
-
-function createRowSubject(row) {
-    // Obtém referência da tabela
-    const table = document.querySelector('#table_subject').querySelector('tbody');
-    // Cria um elemento <tr>
+    const table = document.querySelector('#table_subject');
     const line = document.createElement('tr')
-    // Cria elementos <td> para cada coluna
-    const col_id = document.createElement('td');
-    col_id.textContent = row.id; // Adiciona o ID
-
-    const col_nome = document.createElement('td');
-    col_nome.textContent = row.name; // Adiciona o nome do assunto
-
-    // Adiciona os elementos <td> ao <tr>
+    const col_id = document.createElement('td') 
+    col_id.textContent = id
+    const col_nome = document.createElement('td')     
+    col_nome.textContent = input_nome.value
     line.appendChild(col_id);
     line.appendChild(col_nome);
-
-    // Adiciona a linha <tr> à tabela <tbody>
     table.appendChild(line);
-}
+    data_subjects.push(assunto)
+    drawChart();
 
+
+    const select = document.querySelector('#select_subject');
+    const op = document.createElement("option"); 
+    op.textContent = input_nome.value
+    op.value = input_nome.value
+    select.appendChild(op)
+
+    form .reset()
+}
