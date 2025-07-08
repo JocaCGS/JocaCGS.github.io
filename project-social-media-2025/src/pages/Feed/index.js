b1533ea896328a3110192c6395fa5b77eebd6562
JocaCGS <joaquimcolini06@hotmail.com>
2025-07-08 14:25:57 -0300

    update

--- project-social-media-2025/src/pages/Feed/index.js
+++ project-social-media-2025/src/pages/Feed/index.js
@@ -3,12 +3,39 @@ import './style.css'; // importando o CSS local da página (opcional)
 
 function Feed() {
   return (
-      <div className="feed-container">
-        <div className='feed-configs'></div>
-        <div className='feed-posts'></div>
-        <div className='feed-trending'></div>
+      <div className="wholepage">
+        <div className='configs-div'>
+          <div className='configs-content'>
+            configs
+          </div>
+        </div>
+        <div className='feed-div'>
+          <div className='feed-content1'>
+            <div className='postingdiv'>
+              <h1>Feed</h1>
+              <textarea className='newpost' placeholder='What are you thinking?'>
+                
+              </textarea>
+              <button className='create-post-button'>
+                + 
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className='trending-div'>
+          <div className='trending-content'>
+            trending
+          </div>
+        </div>
     </div>
   );
+}
+
+const hardcodedprofile = {
+  firstName: "Walter",
+  lastName: "White",
+  age: 52,
+  id: 1
 }
 
 export default Feed;
